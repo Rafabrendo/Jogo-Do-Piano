9892db25f284177efb0e8c3ffecf1622bafd12bb
Rafael Brendo <rafalive122@hotmail.com>
2024-03-05 16:11:39 -0300

    Mapeando teclas permitidas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,5 +1,6 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
+let mapedKeys = []; //As teclas são vão tocar se estiverem dentro desse vetor
 let audio = new Audio("src/tunes/a.wav");
 
 const playTune = (key) => {
@@ -18,8 +19,11 @@ const playTune = (key) => {
 pianoKeys.forEach((key)=>{
     console.log(key.dataset.key); //Vai pegar os valores que tem o prefixo data, e dentro uma variavel que tem o nome key
     key.addEventListener("click", ()=> playTune(key.dataset.key));
+    mapedKeys.push(key.dataset.key)
 });
 
 document.addEventListener("keydown", (e)=>{
-    playTune(e.key)
+    if(mapedKeys.includes(e.key)){
+        playTune(e.key);
+    }
 });
